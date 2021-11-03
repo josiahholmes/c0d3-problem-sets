f3c5466a25e0c3b7c8db74d474e4f8d5944a8061
Josiah Holmes <josiahholmes@protonmail.com>
2021-11-03 19:30:18 -0400

    Commit j0 problem sets

---
+++ j0/j0-004.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function called solution that
+ * Takes in 2 numbers, returns the larger number
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+
+const solution = (num1, num2) => {
+    if (num1 > num2) return num1;
+    return num2;
+}
