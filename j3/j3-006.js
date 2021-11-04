846ef16feb5daf8e7382be8ae6ee60156f5adb4a
Josiah Holmes <josiahholmes@protonmail.com>
2021-11-04 17:08:00 -0400

    commit problem j3-p6

---
+++ j3/j3-006.js
@@ -0,0 +1,14 @@
+/*
+ * write a function that takes in an array of numbers, and returns a new array of all duplicate numbers
+ * @param {array} arr
+ * @returns {array}
+*/
+
+const solution = (arr) => {
+    let counts = {};
+    return arr.reduce((acc, elem) => {
+        counts[elem] = (counts[elem] || 0) + 1;
+        if (counts[elem] > 1 && !acc.includes(elem)) acc.push(elem);
+        return acc;
+    }, []);
+}
