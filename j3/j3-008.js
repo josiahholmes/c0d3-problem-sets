294a0da835842c633cd4b8f095bc470e756107d5
Josiah Holmes <josiahholmes@protonmail.com>
2021-11-04 23:04:22 -0400

    commit problem j3p8

---
+++ j3/j3-008.js
@@ -0,0 +1,16 @@
+/*
+ * Write a function that takes in an object and a number (millieseconds).
+ * - You must call each function value of the object {"nVal": (k) => {...}}
+ * - {"nVal": (k) => {...When this function runs, k is "nVal"...}}
+ * @param {object} obj
+ * @param {number} num (millieseconds)
+ * @call each function value of the object, millieseconds after each other
+*/
+
+const solution = (obj, num, keys = Object.keys(obj), i = 0) => {
+    if (i === keys.length) return;
+    obj[keys[i]](keys[i]);
+    setTimeout(() => {
+        solution(obj, num, keys, i + 1);
+    }, num);
+}
